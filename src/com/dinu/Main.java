c11aacd2badcef50f289e3e6dc6955134c04799d
akila <toakila7@gmail.com>
2025-02-13 18:32:56 +0530

    Revert "second commit"
    This reverts commit e16cf40e7a468095d59a85d29f58fd683a4c8d23.

--- src/com/dinu/Main.java
+++ src/com/dinu/Main.java
@@ -3,6 +3,5 @@ package com.dinu;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Hello World 2");
     }
 }
